d3174e2d2833d60910a8e702d14c8ea011725147
Hugo <G00302429@stu.raritanval.edu>
2024-11-03 17:06:17 -0500

    7.19
    added the  averageAccessesPerMonth method

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -25,7 +25,7 @@ public class LogAnalyzer
         hourCounts = new int[24];
         dayCounts = new int[28];
         monthCounts = new int [12];
-        yearCounts = new int [12];
+        yearCounts = new int [5];
         // Create the reader to obtain the data.
         reader = new LogfileReader(logFile);
     }
@@ -40,10 +40,10 @@ public class LogAnalyzer
             int hour = entry.getHour();
             hourCounts[hour]++;
         }
-        
+
         reader.reset();
     }
-    
+
     /**
      * Analyze the Monthly access data from the log file.
      */
@@ -54,11 +54,11 @@ public class LogAnalyzer
             int month = entry.getMonth() - 1;
             monthCounts[month]++;
         }
-        
+
         reader.reset();
     }
-    
-       /**
+
+    /**
      * Analyze the Yearly access data from the log file.
      */
     public void analyzeYearlyData()
@@ -68,23 +68,23 @@ public class LogAnalyzer
             int year = entry.getYear() - 2015;
             yearCounts[year]++;
         }
-        
+
         reader.reset();
     }
-    
+
     /**
      * will analyze year, month, day, and hour
      */
     public void analyzeData(){
-    
-    analyzeYearlyData();
-    analyzeMonthlyData();
-    analyzeHourlyData();
-    analyzeDailyData();
-    
+
+        analyzeYearlyData();
+        analyzeMonthlyData();
+        analyzeHourlyData();
+        analyzeDailyData();
+
     }
-    
-     /**
+
+    /**
      * Analyze the hourly access data from the log file.
      */
     public void analyzeDailyData()
@@ -94,16 +94,15 @@ public class LogAnalyzer
             int day = entry.getDay() - 1;
             dayCounts[day]++;
         }
-        
+
         reader.reset();
     }
-    
+
     /**
      * @returns the day that most people access the site
      */
     public String busiestDay(){
 
-        
         int highestDay = 0;
         int dayCompare = 0;
 
@@ -117,13 +116,12 @@ public class LogAnalyzer
         return String.format("Most people visted the site on day %d",(highestDay + 1) );
 
     }
-    
-     /**
+
+    /**
      * @returns the day that the least people access the site
      */
     public String quietestDay(){
 
-        
         int lowestDay = 0;
         int lowDayCompare = dayCounts[0];
 
@@ -134,12 +132,10 @@ public class LogAnalyzer
             }
         }
 
-       return String.format("Day %d has the least amount of trafic compared to other days",(lowestDay + 1));
+        return String.format("Day %d has the least amount of trafic compared to other days",(lowestDay + 1));
 
     }
-    
-  
-    
+
     
 
     /**
@@ -152,7 +148,7 @@ public class LogAnalyzer
             LogEntry entry = reader.next();
             numberOfLines++;
         }
-        
+
         reader.reset();
         return numberOfLines;
     }
@@ -162,7 +158,6 @@ public class LogAnalyzer
      */
     public String busiestHour(){
 
-        
         int highestHour = 0;
         int hourCompare = 0;
 
@@ -176,13 +171,12 @@ public class LogAnalyzer
         return String.format("Most people accessed the site on hour %d",highestHour );
 
     }
-    
+
     /**
      * @returns the hour that the least people access the site
      */
     public String quietestHour(){
 
-        
         int lowestHour = 0;
         int lowHourCompare = hourCounts[0];
 
@@ -193,17 +187,15 @@ public class LogAnalyzer
             }
         }
 
-       return String.format("Hour %d has the least amount of trafic compared to other hours",lowestHour );
+        return String.format("Hour %d has the least amount of trafic compared to other hours",lowestHour );
 
     }
-    
-    
-     /**
+
+    /**
      * @returns the month that the least people access the site
      */
     public String quietestMonth(){
 
-        
         int lowestMonth = 0;
         int lowMonthCompare = monthCounts[0];
 
@@ -214,54 +206,66 @@ public class LogAnalyzer
             }
         }
 
-       return String.format("Month %d has the least amount of trafic compared to other months",lowestMonth );
+        return String.format("Month %d has the least amount of trafic compared to other months",lowestMonth );
 
     }
-    
+
     /**
      * @returns the month that the most people access the site
      */
     public String busiestMonth(){
 
-        
         int highestMonth = 0;
         int highMonthCompare = monthCounts[0];
 
         for (int i = 0; i < monthCounts.length; i++ ){
             if(monthCounts[i] > highMonthCompare ){
                 highMonthCompare = monthCounts[i];
-               highestMonth = i + 1;
+                highestMonth = i + 1;
             }
         }
 
-       return String.format("Month %d has the most amount of trafic compared to other months",highestMonth );
+        return String.format("Month %d has the most amount of trafic compared to other months",highestMonth );
 
     }
     
+    /**
+     * This method takes the total amount of times the site is visted on each month then divides it by the
+     * amount of years that are tracked to get the avrage amount of times the website is visted each month a year
+     */
+    public void averageAccessesPerMonth(){
+
+        System.out.println("Month: Avg Count");
+        for(int month = 0; month < monthCounts.length; month++){
+
+            System.out.println((month +1) + ": " + (monthCounts[month]/yearCounts.length));
+
+    
+        }
+
+    }
+
     
     /**
      * Method that finds out which two hour block had the most trafic
      * @returns A string that details the two hour block that had most trafic
      */
     public String busiestTwoHour(){
-    
-    
+
         int highHourCompare = (hourCounts[0] + hourCounts[23]);
         int highHourA = 0;
         int highHourB = 23;
-        
+
         for(int i =0; i < hourCounts.length - 1; i++){
-        if (hourCounts[i] + hourCounts[i +1] > highHourCompare ){
-        highHourCompare = hourCounts[i] + hourCounts[i +1];
-        highHourA = i;
-         highHourB = i + 1;
-        }
-        
-        
+            if (hourCounts[i] + hourCounts[i +1] > highHourCompare ){
+                highHourCompare = hourCounts[i] + hourCounts[i +1];
+                highHourA = i;
+                highHourB = i + 1;
+            }
+
         }
         return String.format("The busiest two hour block is between the hours of %d and %d",highHourA, highHourB);
-    
-    
+
     
     }
 
@@ -277,21 +281,19 @@ public class LogAnalyzer
             System.out.println(hour + ": " + hourCounts[hour]);
         }
     }
-    
+
     /**
      * Will print how many times each month had access
      */
     public void totalAccessesPerMonth(){
-    System.out.println("Month: Count");
-    for(int month = 0; month < monthCounts.length; month++){
-    
-    System.out.println((month +1) + ": " + monthCounts[month]);
-    
-    }
-    
-    
-    }
+        System.out.println("Month: Count");
+        for(int month = 0; month < monthCounts.length; month++){
 
+            System.out.println((month +1) + ": " + monthCounts[month]);
+
+        }
+
+    }
     /**
      * Print the lines of data read by the LogfileReader
      */
